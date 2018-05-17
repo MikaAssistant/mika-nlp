10e6f5aa77d75099b62b9fafd3ed4ae583379957
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2018-05-17 20:29:39 -0300

    implementado as intencoes incompletas

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
 'use strict';
 const env = require('dotenv').config().parsed;
 const socket = require('socket.io-client')('http://'+env.HTTP_HOST+':'+env.HTTP_PORT);
-const apiai = require('apiai');
-const dialogflow = apiai(env.DIALOGFLOW_CLIENT_ACCESS_TOKEN);
+const dialogflow = require('./src/dialogflow');
 
-socket.on('nlp',function(msg){
-    console.log('Enviando + '+ msg);
-
-    let request = dialogflow.textRequest(msg, {
-        sessionId: '2'
-    });
-
-    request.on('response', function(response) {
-        let resp = {};
-        resp.intent = response.result.metadata.intentName;
-        resp.action = response.result.action;
-        resp.parameters = response.result.parameters;
-        console.log(resp);
-        socket.emit('kernel',resp);
-    });
-
-    request.on('error', function(error) {
-        console.log(error);
-    });
-    request.end();
-});
+async function main () {
+    socket.on('nlp',async function(mensagem){
+        let nlp = await dialogflow.send(mensagem);
+        if(nlp.actionIncomplete === true){
+            console.log('client', nlp.mensagem)
+            socket.emit('client',nlp.mensagem);
+        }
+        if(nlp.actionIncomplete === false){
+            console.log('kernel', nlp)
+            socket.emit('kernel',nlp);
+        }
+    })
+}
+main();
 
 console.log('Mika NLP Running');
---
+++ src/dialogflow.js
@@ -0,0 +1,36 @@
+'use strict';
+const env = require('dotenv').config().parsed;
+const uuidv4 = require('uuid/v4');
+let id = uuidv4();
+
+//DIALOGFLOW
+const Apiai = require('apiai');
+const apiai = Apiai(env.DIALOGFLOW_CLIENT_ACCESS_TOKEN);
+
+let contexts = null;
+
+const Request = {
+    send: (mensagem) => {
+        return new Promise(resolve => {
+            let request = apiai.textRequest(mensagem, {
+                sessionId: id,
+                contexts: contexts
+            });
+    
+            request.on('response', function(response) {
+                contexts = response.result.contexts;
+                let r = {
+                    mensagem: response.result.fulfillment.speech,
+                    intent: response.result.metadata.intentName,
+                    action: response.result.action,
+                    actionIncomplete: response.result.actionIncomplete,
+                    parameters: response.result.parameters
+                }
+                resolve(r);
+            });
+            request.end();
+        });
+    }
+}
+
+module.exports = Request;
